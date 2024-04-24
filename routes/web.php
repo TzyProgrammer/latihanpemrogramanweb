1e254ed600632ad7289e54110237974df646e49c
TzyProgrammer <classroomtarich@gmail.com>
2024-04-24 23:12:27 +0700

    Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListBarangController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -9,3 +10,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID ' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});
+
+// Route::get('/listbarang/{id}/{nama}', function($id,$nama){
+//     return view('list_barang', compact('id', 'nama'));
+// });
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
